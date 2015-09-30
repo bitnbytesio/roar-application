66b1a7067343a67ad4065016ffd6871ec92c9ff6
Harcharan Singh <artisangang@gmail.com>
2015-09-30 21:11:10 +0530

    init

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+define('ROAR_START', microtime(true));
+
+define('BASE_PATH', __DIR__);
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('PUBLIC_PATH'))
+	define('PUBLIC_PATH', __DIR__);
+
+require __DIR__. '/../vendor/autoload.php';
+
+require __DIR__ . '/../bootstrap.php';
